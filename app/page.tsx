f46748086e0ffc807e53d4d6d2d4a855f3754ed7
martindesktop <martinlucam@gmail.com>
2025-09-25 23:37:41 -0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -2,35 +2,38 @@ export default function Home() {
   return (
 <div>
 
-  <header className="absolute top-0 left-0 w-full bg-[#02ff71] text-black p-4 z-10">
-  <h1 className="text-2xl font-bold">asdfa</h1>
-  </header>
-
   <main className="relative h-screen flex">
 
 
       <div className="flex flex-col justify-center h-screen pl-[400px] gap-0">
-        <div id="titlePlate" className="-mb-20">Martin</div>
-        <div id="titlePlate">Cam</div>
-      </div>
+         <div className="flex gap-4 mb-8">
+            <a 
+              href="https://github.com"
+              target="_blank"
+              className="text-gray-800 hover:text-gray-600 transition-colors duration-200"
+              
+            ><img src="\icons\github-142-svgrepo-com.svg" alt="Github" className="w-12 h-12 hover:scale-110 transition-transform"></img></a>
+          </div>
 
+          <div id="titlePlate" className="-mb-20">Martin</div>
+          <div id="titlePlate">Cam</div>
+          </div>
 
-
-      <div
-        style={{
-        position: "absolute",
-        top: -50,
-        left: 1100,
-        width: "100%",
-        height: "100%",
-        transform: "rotate(-45deg)",
-        transformOrigin: "top left",
-        background: "#E79FFF",
-        borderRadius: "21.42px",
-        zIndex: -1,
-       }}
-        ></div>
-
+          <div
+            style={{
+            position: "absolute",
+            top: -50,
+            left: 1100,
+            width: "100%",
+            height: "100%",
+            transform: "rotate(-45deg)",
+            transformOrigin: "top left",
+            background: "#E79FFF",
+            borderRadius: "21.42px",
+            zIndex: -1,
+              }}
+          ></div>
+            
 
 
 
