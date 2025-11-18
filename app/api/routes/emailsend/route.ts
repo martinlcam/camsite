05f3d267d688031846d02b60c07a8db750c512eb
Martin <martinlucam@gmail.com>
2025-11-18 13:54:56 -0800

    fix: missing api key, will switch to envx

--- app/api/routes/emailsend/route.ts
+++ app/api/routes/emailsend/route.ts
@@ -1,7 +1,7 @@
 //import { NextRequest, NextResponse } from 'next/server';
-import { Resend } from 'resend';
+//import { Resend } from 'resend';
 //import { ContactEmailTemplate } from '../../../components/ContactEmailTemplate';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+//const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(){}
